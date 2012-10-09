5332fa8f558d0613144185442f30423051aacb0c
John Cowie <john.a.cowie@gmail.com>
2012-10-09 17:02:11 +0100

    #0 - Ilias, John - Using dropbox and checkboxes field in our form

--- src/main/java/jmdb/tutorial/dropwizard/domain/applicationform/applicant/ApplicantName.java
+++ src/main/java/jmdb/tutorial/dropwizard/domain/applicationform/applicant/ApplicantName.java
@@ -1,16 +1,35 @@
 package jmdb.tutorial.dropwizard.domain.applicationform.applicant;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
 public class ApplicantName {
 
     private final String firstName;
     private final String lastName;
 
-    public ApplicantName(String firstName, String lastName) {
+    private final String favouriteColor;
+    private final List<String> favouriteBooks;
+
+    public ApplicantName(String firstName, String lastName, String favouriteColor, List<String> favouriteBooks) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.favouriteColor = favouriteColor;
+        this.favouriteBooks = favouriteBooks;
+    }
+
+    public ApplicantName(String firstName, String lastName, String favouriteColor) {
+        this(firstName, lastName, favouriteColor, new ArrayList<String>());
     }
 
     public String toString() {
-        return "firstName: " + firstName + ", lastName: " + lastName;
+        return ReflectionToStringBuilder.toString(this);
     }
+
 }
